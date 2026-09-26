66f80af1d57963c712e996047e68a7b294c676a7
caithesai <cmcdonnell1@csuchico.edu>
2026-09-25 18:32:34 -0700

    fuckig

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+string CheckColor();
+bool CheckWhite();
+bool CheckBlack();
 
 struct RGB
 {
@@ -7,9 +12,37 @@ struct RGB
 	int GREEN;
 	int BLUE;
 };
+RGB clr;
 
 int main()
 {
+	if ((red - green) > 50 && (red - green) > 50 && !CheckBlack() && !CheckWhite() )
+	{
+		return "shutup";
+	}
 
-  return 0;
+}
+
+bool CheckWhite()
+{
+	if (clr.RED >= 224 && clr.GREEN >= 224 && clr.BLUE >= 224)
+	{
+		return true
+	}
+	else
+	{
+		return false
+	}
+}
+
+bool CheckBlack()
+{
+	if (clr.RED <= 32 && clr.GREEN <= 32 && clr.BLUE <= 32)
+	{
+		return true
+	}
+	else
+	{
+		return false
+	}
 }
